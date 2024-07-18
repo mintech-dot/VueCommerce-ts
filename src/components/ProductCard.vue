da2f2ebeef7493043cd10aaa3ad3d7d3c1e1d2bb
mintech-dot <mintech@protonmail.com>
2024-07-18 11:11:28 +0100

    refactor: make product card reusable and implement it

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,30 +1,58 @@
 <template>
-  <div class="flex border-secondary/50 border rounded-xl">
-    <div class="w-1/2">
-      <img :src="Card" class="h-full rounded-xl" alt="" />
-    </div>
-
-    <div class="w-1/2 px-4 pt-4">
-      <h1 class="text-secondary">Product Title</h1>
-      <img :src="rating" alt="" />
-      <h2 class="text-[#121212] text-medium font-inter font-medium">$89.99</h2>
-      <p class="text-[#121212] text-base font-inter font-normal text-wrap py-4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non erat
-      </p>
-      <div>
-        <Button variant="primary" size="lg">Add to cart</Button>
+  <div
+    v-if="product && product.items"
+    class="px-4 md:px-8 lg:px-24 pt-12 gap-4 grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1"
+  >
+    <div
+      v-for="product in product.items"
+      :key="product.id"
+      class="flex border-secondary/50 border rounded-xl"
+    >
+      <div class="w-1/2">
+        <img
+          v-if="product.featuredAsset"
+          :src="product.featuredAsset.preview"
+          :alt="product.name"
+          class="h-full rounded-l-xl"
+        />
       </div>
-      <div class="flex gap-2 justify-center">
-        <img :src="heart" alt="" class="w-8" />
-        <h2 class="py-4">wishlist</h2>
+
+      <div class="w-1/2 p-4">
+        <img :src="rating" class="pb-4 pt-3" alt="" />
+
+        <h1 class="text-primary font-semibold font-inter pb-1">{{ product.name }}</h1>
+        <h2 class="text-[#121212] text-[14px] font-inter font-semibold">
+          $ {{ product.variants[0].priceWithTax }}
+        </h2>
+        <p
+          class="text-secondary text-base font-inter font-normal text-wrap pt-4 line-clamp-5 "
+        >
+          {{ product.description }}
+        </p>
+        <div class="mt-6">
+          <Button variant="primary" size="lg">Add to cart</Button>
+        </div>
+        <div class="flex gap-2 justify-center">
+          <img :src="heart" alt="" class="w-8" />
+          <h2 class="py-4">wishlist</h2>
+        </div>
       </div>
     </div>
   </div>
+  <p v-else-if="loading">Loading...</p>
+  <p v-else-if="error">No products found.</p>
+  <p v-else class="text-center">No products found.</p>
 </template>
 
 <script setup lang="ts">
-import Card from "../assets/Card.png";
 import rating from "../assets/rating.svg";
 import Button from "../components/ui/Button.vue";
 import heart from "../assets/heart.svg";
+
+type ProductCardProps = {
+  product: any;
+  loading: boolean;
+  error: any;
+};
+const props = defineProps<ProductCardProps>();
 </script>
