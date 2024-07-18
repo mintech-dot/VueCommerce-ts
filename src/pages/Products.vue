d28e886d5f1f20893844bb950d9c1a43803b4bc8
mintech-dot <mintech@protonmail.com>
2024-07-18 07:40:57 +0100

    feat: add Products page

---
+++ src/pages/Products.vue
@@ -0,0 +1,16 @@
+<template>
+    <Navbar />
+    <Hero />
+    <ProductList />
+    <Newsletter />
+    <Footer />
+  </template>
+  
+  <script setup lang="ts">
+  import Navbar from "../layouts/Navbar.vue";
+  import Hero from "../views/Hero.vue";
+  import ProductList from "../views/ProductsList.vue";
+  import Newsletter from "../views/Newsletter.vue";
+  import Footer from "../layouts/Footer.vue";
+  </script>
+  
