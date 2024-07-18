75904a16da328f56699d2cddcb2fbcea7c5d7f9e
mintech-dot <mintech@protonmail.com>
2024-07-18 07:53:24 +0100

    feat: add products page path to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import products from "./pages/Products.vue";
 </script>
 
 <template>
-  <HelloWorld msg="Welcome to Izar" />
+  <products />
 </template>
-
