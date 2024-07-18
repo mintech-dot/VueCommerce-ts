0cb6911f4dab7baef07486713f5746d855274681
mintech-dot <mintech@protonmail.com>
2024-07-18 07:39:35 +0100

    feat: add Hero, NewsLetter and ProductsList sections

---
+++ src/views/ProductsList.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="px-4 md:px-8 lg:px-24 pt-12 flex justify-between gap-8">
+      <div class="sm:flex  md:justify-between gap-8 w-screen">
+        <div class="sm:flex gap-8">
+          <div>
+            <h1 class="text-secondary mt-4">CATEGORIES</h1>
+            <Dropdown :options="productOptions" />
+          </div>
+          <div>
+            <h1 class="text-secondary mt-4">FILTER BY</h1>
+            <Dropdown :options="FilterOptions" />
+          </div>
+        </div>
+        <div >
+          <h1 class="text-secondary mt-4">FILTER BY</h1>
+          <input
+            class="border border-secondary/50 rounded-[8px] px-4 py-2 w-full"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
+      </div>
+    </div>
+    <div
+      class="px-4 md:px-8 lg:px-24 pt-12 gap-4 grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1"
+    >
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+    <div div class="flex justify-center py-8">
+      <Button variant="secondary" size="lg">Show More</Button>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import ProductCard from "../components/ProductCard.vue";
+  import { ref } from "vue";
+  import Dropdown from "../components/ui/Dropdown.vue";
+  import Button from "../components/ui/Button.vue";
+  interface Option {
+    value: string;
+    text: string;
+  }
+  
+  const productOptions = ref<Option[]>([
+    { value: "laptop", text: "laptop" },
+    { value: "product2", text: "Product 2" },
+    { value: "product3", text: "Product 3" },
+  ]);
+  
+  const FilterOptions = ref<Option[]>([
+    { value: "A-z", text: "A-z" },
+    { value: "product2", text: "Product 2" },
+    { value: "product3", text: "Product 3" },
+  ]);
+  </script>
+  
