c59c0505469fd61e5d2f070a2d0a37fd61dd624f
mintech-dot <mintech@protonmail.com>
2024-07-24 15:23:15 +0100

    feat(): Update useCart hook

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -4,8 +4,14 @@ import { useQuery } from "@vue/apollo-composable";
 
 const productsQuery = useQuery<GetProductsQuery>(GetProductsDocument);
 
+interface CartItem {
+  productId: string;
+  quantity: number;
+  priceWithTax?: number;
+}
+
 export const useCart = () => {
-  const CartItems = ref<string[]>([]);
+  const CartItems = ref<CartItem[]>([]);
 
   const load = () => {
     const ProductInCartStorage = JSON.parse(
@@ -20,39 +26,87 @@ export const useCart = () => {
 
   const getProductsInCart = () => {
     const allProducts = productsQuery.result.value?.products.items || [];
-    return allProducts.filter((product) =>
-      CartItems.value.includes(product.id)
-    );
+    return CartItems.value.map(cartItem => {
+      const product = allProducts.find(product => product.id === cartItem.productId);
+      return {
+        ...product,
+        quantity: cartItem.quantity,
+        priceWithTax: product?.variants[0].priceWithTax,
+      };
+    }).filter(product => product !== undefined);
   };
 
   const searchProductsInCart = (searchTerm: string) => {
     const productsInCart = getProductsInCart();
     return productsInCart.filter((product) =>
-      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+      product.name?.toLowerCase().includes(searchTerm.toLowerCase())
     );
   };
 
   const addToCart = (productId: string) => {
-    if (!CartItems.value.includes(productId)) {
-      CartItems.value.push(productId);
+    const existingProduct = CartItems.value.find(
+      (item) => item.productId === productId
+    );
+
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+    } else {
+      CartItems.value.push({ productId, quantity: 1 });
+    }
+
+    save();
+  };
+
+  const removeProductfromCart = (productId: string) => {
+    CartItems.value = CartItems.value.filter(
+      (item) => item.productId !== productId
+    );
+    save();
+  };
+  const incrementQuantity = (productId: string) => {
+    const product = CartItems.value.find(item => item.productId === productId);
+    if (product) {
+      product.quantity += 1;
       save();
     }
   };
 
-  const removeProductfromCart = (productId: string) => {
-    CartItems.value = CartItems.value.filter((id) => id !== productId);
-    save();
+  const multiplyQuantities = (multiplier: number, price : number) => {
+    return (price * multiplier).toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    })
   };
 
+  const totalPrice = computed(() => {
+    let total = 0
+    const CartItems = getProductsInCart();
+    CartItems.forEach(item => total += item.priceWithTax * item.quantity);
+    return total.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  });
 
+  const decrementQuantity = (productId: string) => {
+    const product = CartItems.value.find(item => item.productId === productId);
+    if (product && product.quantity > 1) {
+      product.quantity -= 1;
+      save();
+    } 
+  };
   load();
 
   return {
-    AddToCart: computed(() => CartItems.value),
+    CartItems: computed(() => CartItems.value),
     addToCart,
     removeProductfromCart,
     getProductsInCart,
     searchProductsInCart,
+    incrementQuantity,
+    decrementQuantity,
+    multiplyQuantities,
+    totalPrice,
     save,
   };
 };
