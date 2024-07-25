a4e7343ea4a37f0b57ff09db022d3c56f9013354
mintech-dot <mintech@protonmail.com>
2024-07-25 23:20:05 +0100

    feat: add application title & htmlAttrs

--- izar/nuxt.config.ts
+++ izar/nuxt.config.ts
@@ -1,6 +1,15 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "eng",
+        dir: "ltr",
+      },
+      title: "Izar - Your Stellar Shopping Experience ",
+    },
+  },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   modules: ["@nuxtjs/apollo", "nuxt-graphql-client"],
