6f9e3134b066b769f36b24efabea9f304ade314f
mintech-dot <mintech@protonmail.com>
2024-07-27 21:15:13 +0100

    feat: add CartTable, CheckoutSteps, SummaryCard components and cart page

---
+++ izar-nuxt/components/CheckoutStpes.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex justify-between mt-8 mx-14 font-inter">
+    <div class="flex gap-1">
+      <h2 class="text-white text-xl bg-primary rounded-full my-2 px-2 -mt-1">
+        1
+      </h2>
+      <h3 class="text-primary font-medium ">Shooping cart</h3>
+    </div>
+    <div class="flex gap-1">
+      <h2 class="text-white text-xl bg-secondary rounded-full my-2 px-2 -mt-1">
+        2
+      </h2>
+      <h3 class="text-secondary">Checkout details</h3>
+    </div>
+    <div class="flex gap-1">
+      <h2 class="text-white text-xl bg-secondary rounded-full my-2 px-2 -mt-1">
+        3
+      </h2>
+      <h3 class="text-secondary">Order complete</h3>
+    </div>
+  </div>
+</template>
